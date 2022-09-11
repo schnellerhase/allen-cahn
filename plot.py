ab6dca37c9090320b04ed28a83d1cd0439182e0c
schnellerhase <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2022-09-11 14:02:58 +0200

    black

--- plot.py
+++ plot.py
@@ -5,17 +5,19 @@ import argparse
 
 pv.start_xvfb(wait=0.1)
 parser = argparse.ArgumentParser()
-parser.add_argument('file', type=str)
+parser.add_argument("file", type=str)
 file = parser.parse_args().file
 
-pv.global_theme.cmap = 'jet'
+pv.global_theme.cmap = "jet"
 pv.global_theme.transparent_background = True
 pv.global_theme.font.size = 10
 
 
 mesh = pv.read(file)
 pl = pv.Plotter(window_size=[1920, 1920], off_screen=True)
-mesh = pl.add_mesh(mesh, scalar_bar_args=dict(width=.9, position_x=(1-.9)/2), clim=[-1, 1])
+mesh = pl.add_mesh(
+    mesh, scalar_bar_args=dict(width=0.9, position_x=(1 - 0.9) / 2), clim=[-1, 1]
+)
 # mesh.GetTitleTextProperty(0)
 
 # mesh.SetUse2DMode(1)
@@ -23,9 +25,9 @@ mesh = pl.add_mesh(mesh, scalar_bar_args=dict(width=.9, position_x=(1-.9)/2), cl
 # The path is for ubuntu
 # mesh.SetFontFile("/usr/share/texlive/texmf-dist/fonts/truetype/public/dejavu/DejaVuSans.ttf")
 pl.view_isometric()
-pl.set_position([.5, .5, 1])
+pl.set_position([0.5, 0.5, 1])
 pl.set_viewup([0, 1, 0])
 # pl.camera.zoom('tight')
 pl.camera.tight()
-pl.save_graphic(f'{file}.svg')
-pl.show(screenshot=f'{file}.png')
+pl.save_graphic(f"{file}.svg")
+pl.show(screenshot=f"{file}.png")
