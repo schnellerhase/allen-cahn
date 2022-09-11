4b3f34c3a6b24d65d7093dc919a41129ea671605
schnellerhase <xDNdnQoyTcrrMdWCVZft4YLqkaRWnRb@protonmail.com>
2022-09-11 19:12:04 +0200

    tight not avaiable in python < 3

--- plot.py
+++ plot.py
@@ -12,22 +12,16 @@ pv.global_theme.cmap = "jet"
 pv.global_theme.transparent_background = True
 pv.global_theme.font.size = 10
 
-
 mesh = pv.read(file)
 pl = pv.Plotter(window_size=[1920, 1920], off_screen=True)
 mesh = pl.add_mesh(
     mesh, scalar_bar_args=dict(width=0.9, position_x=(1 - 0.9) / 2), clim=[-1, 1]
 )
-# mesh.GetTitleTextProperty(0)
 
-# mesh.SetUse2DMode(1)
-# mesh.SetFontFamily(4)
-# The path is for ubuntu
-# mesh.SetFontFile("/usr/share/texlive/texmf-dist/fonts/truetype/public/dejavu/DejaVuSans.ttf")
 pl.view_isometric()
 pl.set_position([0.5, 0.5, 1])
 pl.set_viewup([0, 1, 0])
-pl.camera.zoom('tight')
-# pl.camera.tight()
+if sys.version_info[0] > 2:
+    pl.camera.zoom('tight')
 pl.save_graphic(f"{file}.svg")
 pl.show(screenshot=f"{file}.png")
